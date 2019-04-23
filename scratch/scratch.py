b3d32789db3139e5ef6f8f26b96157583051a507
Ubuntu <ubuntu@ip-172-31-17-56.eu-west-1.compute.internal>
2019-04-23 20:26:47 +0000

    Add token claim validation to Python scratch example

--- scratch/scratch.py
+++ scratch/scratch.py
@@ -1,5 +1,6 @@
 import os
 import json
+import datetime
 
 import requests
 from jwcrypto.jwk import JWK
@@ -7,10 +8,25 @@ from jwcrypto.jwt import JWT
 
 
 raw_token = open('temp-token.txt').read()
-jwks_resp = requests.get(os.environ['OAUTH_SERVER'] + '/keys')
+jwks_resp = requests.get(os.environ['OAUTH_SERVER'] + '/v1/keys')
 
 first_key = jwks_resp.json()['keys'][0]
 key = JWK.from_json(json.dumps(first_key))
 
-token = JWT(jwt=raw_token, key=key)
-print(token.claims)
+# Validate token as per procedure described in:
+# https://developer.okta.com/authentication-guide/tokens/validating-id-tokens/#verify-the-claims
+
+# Use built-in claim validation for exact matching
+# https://jwcrypto.readthedocs.io/en/latest/jwt.html
+expected_claims = {
+   "iss": os.environ['OAUTH_SERVER'],
+   "aud": os.environ['CLIENT_ID']
+}
+
+# Creating JWT object also validates the signature
+token = JWT(jwt=raw_token, key=key, check_claims=expected_claims)
+claims = json.loads(token.claims)
+
+# Perform issued at and expiration time checks
+print('IAT validation: ' + str(datetime.datetime.fromtimestamp(claims['iat']) < datetime.datetime.now()))
+print('EXP validation: ' + str(datetime.datetime.fromtimestamp(claims['exp']) > datetime.datetime.now()))
