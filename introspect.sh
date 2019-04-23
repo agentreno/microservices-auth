b5b05cd26ea98494fac3254703a02e058c296de2
Ubuntu <ubuntu@ip-172-31-17-56.eu-west-1.compute.internal>
2019-04-23 19:26:47 +0000

    Fix typo in introspect endpoint

--- introspect.sh
+++ introspect.sh
@@ -1,4 +1,4 @@
 http --verbose \
     -a $CLIENT_ID:$CLIENT_SECRET \
-    --form POST $OAUTH_SERVER/instropect \
+    --form POST $OAUTH_SERVER/introspect \
     token=$TOKEN token_type_hint=access_token
